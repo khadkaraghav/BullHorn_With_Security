b9e804686c03b6341d5867c71738200a09232f6a
khadkaraghav <khadkaraghav@gmail.com>
2019-03-22 17:10:24 -0400

    First Commit

---
+++ src/main/java/com/example/demo/BullhornSecurityWeek7ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullhornSecurityWeek7ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullhornSecurityWeek7ChallengeApplication.class, args);
+    }
+
+}
